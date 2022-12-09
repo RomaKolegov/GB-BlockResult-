c48cd32267ccfd7cb2a0d35cd26a2269b16e40f9
RomanKolegov <kolegov_rv@mail.ru>
2022-12-10 01:21:19 +0500

    add print array function

--- resultDevelop/Program.cs
+++ resultDevelop/Program.cs
@@ -30,3 +30,18 @@ string[] GenerateArrayFromUser(int length)
     }
     return array;
 }
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
